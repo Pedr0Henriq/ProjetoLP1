b41200ff57140a13dbedc8e37ae77108934b3f8f
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-08 14:33:12 -0300

    Create Tenis.java

---
+++ Tenis.java
@@ -0,0 +1,5 @@
+public class Tenis extends Produto {
+    public Tenis(String modelo, String cor, String tamanho, double preco, int estoque) {
+        super(modelo, cor, tamanho, preco, estoque);
+    }
+}

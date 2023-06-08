bd6f48357765342ceaa56d119f48b65b1a10be62
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-08 14:32:35 -0300

    Create Bermuda.java

---
+++ Bermuda.java
@@ -0,0 +1,5 @@
+public class Bermuda extends Produto {
+    public Bermuda(String modelo, String cor, String tamanho, double preco, int estoque) {
+        super(modelo, cor, tamanho, preco, estoque);
+    }
+}

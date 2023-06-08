ad4dc94f1356bfed990099801eb57cc5e07f6a7a
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-08 14:32:43 -0300

    Create Camisa.java

---
+++ Camisa.java
@@ -0,0 +1,5 @@
+public class Camisa extends Produto {
+    public Camisa(String modelo, String cor, String tamanho, double preco, int estoque) {
+        super(modelo, cor, tamanho, preco, estoque);
+    }
+}

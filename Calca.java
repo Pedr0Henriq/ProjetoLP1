9a98a1739159debe5d6d44da74f6a6eacbacfd41
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-08 14:32:40 -0300

    Create Calca.java

---
+++ Calca.java
@@ -0,0 +1,5 @@
+public class Calca extends Produto {
+    public Calca(String modelo, String cor, String tamanho, double preco, int estoque) {
+        super(modelo, cor, tamanho, preco, estoque);
+    }
+}

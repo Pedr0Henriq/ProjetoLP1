ce73343c162c929504892997eab9f06dabfd88ef
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-08 23:04:30 -0300

    Update Carrinho.java

--- Carrinho.java
+++ Carrinho.java
@@ -24,6 +24,17 @@ public class Carrinho {
         return total;
     }
 
+    public void efetuarCompra() {
+        
+        for (Produto produto : produtos) {
+            int estoqueAtual = produto.getEstoque();
+            produto.setEstoque(estoqueAtual - 1);
+            
+        }
+        System.out.println("Compra efetuada com sucesso!");
+        produtos.clear();
+    }
+
     public void exibirCarrinho() {
         System.out.println("Carrinho de Compras:");
         for (Produto produto : produtos) {

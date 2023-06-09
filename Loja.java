818c3f782f07a55eb9d9db61645d095739579534
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-09 14:57:01 -0300

    Update Loja.java

--- Loja.java
+++ Loja.java
@@ -20,8 +20,10 @@ public class Loja {
                             for()*/
                             break;
                         case 1:
+
                             break;
                         case 2:
+                            Gerente.exibirInformacoes();
                             break;
                         case 3:
                             break;
@@ -31,15 +33,30 @@ public class Loja {
                 }
                 break;
             case 1:
+                Gerente g1= new Gerente();
+                //System.out.println("Digite seu cpf: ");
                 System.out.println("O que voce  deseja fazer: 0-Adicionar novo produto  1-Remover um produto  2-Editar produto");
                 escolha=sc.nextInt();
                 while(escolha>=0&&escolha<3){
                     switch(escolha){
                     case 0:
+                        g1.adicionaPEstoque();
+                        System.out.println("Lista de Produtos Atualizadas!");
+                        Gerente.exibirInformacoes();
+                        System.out.println("O que voce  deseja fazer: 0-Adicionar novo produto  1-Remover um produto  2-Editar produto");
+                        escolha=sc.nextInt();
                         break;
                     case 1:
+                        g1.removePEstoque();
+                        System.out.println("Lista de Produtos Atualizadas!");
+                        Gerente.exibirInformacoes();
+                        System.out.println("O que voce  deseja fazer: 0-Adicionar novo produto  1-Remover um produto  2-Editar produto");
+                        escolha=sc.nextInt();
                         break;
                     case 2:
+                        g1.editaPEstoque();
+                        System.out.println("O que voce  deseja fazer: 0-Adicionar novo produto  1-Remover um produto  2-Editar produto");
+                        escolha=sc.nextInt();
                         break;
                     default:
                         break;

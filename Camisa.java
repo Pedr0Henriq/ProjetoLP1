5d0c0700e6b0eab9340e351dbadf11e092d970ae
Pedro Henrique <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-12 14:14:48 -0300

    Delete Camisa.java

--- Camisa.java
+++
@@ -1,5 +0,0 @@
-public class Camisa extends Produto {
-    public Camisa(String modelo, String cor, String tamanho, double preco, int estoque) {
-        super(modelo, cor, tamanho, preco, estoque);
-    }
-}

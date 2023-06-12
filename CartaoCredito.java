32bce9abaa017dc2b971b40a105a85d81f155ea8
pedro.lima <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-12 11:00:45 -0300

    Update CartaoCredito.java

--- CartaoCredito.java
+++ CartaoCredito.java
@@ -7,25 +7,30 @@ public class CartaoCredito {
         if(c1.calcularTotal()<=100.0){
             System.out.println("Podemos parcelar em ate 2 vezes, digite a quantidade de parcelas que voce deseja: ");
             sc=new Scanner(System.in);
-            while(sc.nextInt()>2){
-            System.out.println("Numero de parcelas invalido! Digite novamente: ");  }
-            parcelas=sc.nextInt();}
+            parcelas=sc.nextInt();
+            while(parcelas>2){
+            System.out.println("Numero de parcelas invalido! Digite novamente: "); 
+            parcelas=sc.nextInt(); }
+}
         else{
             if(c1.calcularTotal()>100.0 && c1.calcularTotal()<=250.0){
             System.out.println("Podemos parcelar em ate 4 vezes, digite a quantidade de parcelas que voce deseja: ");
             sc=new Scanner(System.in);
-            while(sc.nextInt()>4){
-                System.out.println("Numero de parcelas invalido! Digite novamente: ");
-            }
             parcelas=sc.nextInt();
+            while(parcelas>4){
+                System.out.println("Numero de parcelas invalido! Digite novamente: ");
+                parcelas=sc.nextInt();
+            }
+            
             }
             else{
                 System.out.println("Podemos parcelar em ate 6 vezes, digite a quantidade de parcelas que voce deseja: ");
             sc=new Scanner(System.in);
-            while(sc.nextInt()>6){
-                System.out.println("Numero de parcelas invalido! Digite novamente: ");
-            }
             parcelas=sc.nextInt();
+            while(parcelas>6){
+                System.out.println("Numero de parcelas invalido! Digite novamente: ");
+                parcelas=sc.nextInt();
+            }
             }
         }
         return parcelas;

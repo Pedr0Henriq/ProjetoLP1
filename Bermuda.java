7ed93ff43339be27197103ae7c6b486c0477806a
Pedro Henrique <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-12 14:14:28 -0300

    Delete Bermuda.java

--- Bermuda.java
+++
@@ -1,5 +0,0 @@
-public class Bermuda extends Produto {
-    public Bermuda(String modelo, String cor, String tamanho, double preco, int estoque) {
-        super(modelo, cor, tamanho, preco, estoque);
-    }
-}

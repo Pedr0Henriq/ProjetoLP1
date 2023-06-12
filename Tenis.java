96337730a6183e135a0e2e06e5a4e4707a183b76
Pedro Henrique <116588159+Pedr0Henriq@users.noreply.github.com>
2023-06-12 14:15:00 -0300

    Delete Tenis.java

--- Tenis.java
+++
@@ -1,5 +0,0 @@
-public class Tenis extends Produto {
-    public Tenis(String modelo, String cor, String tamanho, double preco, int estoque) {
-        super(modelo, cor, tamanho, preco, estoque);
-    }
-}
